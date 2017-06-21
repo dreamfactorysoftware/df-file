127663f955b69b38b9cb0c48739b92c009b78542
Lee Hicks <leehicks@dreamfactory.com>
2017-06-21 15:43:33 -0400

    Local file service config now accepts paths relative to the storage folder as well as full system paths.

--- src/Models/LocalFileConfig.php
+++ src/Models/LocalFileConfig.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DreamFactory\Core\File\Models;
 
 class LocalFileConfig extends FilePublicPath
@@ -11,8 +12,9 @@ class LocalFileConfig extends FilePublicPath
         switch ($schema['name']) {
             case 'container':
                 $schema['label'] = 'Root Folder';
-                $schema['default'] = storage_path('app');
-                $schema['description'] = 'Enter a full path for a root folder.';
+                $schema['description'] = 'Enter a full folder path from your system root, ' .
+                    'or a path relative to the configured storage folder for this installation, i.e. ' . storage_path() .
+                    '. This path must be readable and writable by the web server.';
                 break;
             default:
                 parent::prepareConfigSchemaField($schema);
